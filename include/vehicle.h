68fbc052b8e324297c796dd79d1920dcf4eaa9bb
OllieReynolds <cubed@outlook.com>
2016-12-20 20:11:42 +0000

    Cleanup

--- include/vehicle.h
+++ include/vehicle.h
@@ -3,47 +3,32 @@
 #include <vector>
 
 #include "draw_line.h"
-#include "sensor.h"
+#include "drawable.h"
 #include "global_data.h"
+#include "utils.h"
 
 namespace simulation {
 	struct Vehicle : Drawable {
 		Vehicle() : Drawable(Transform(), 0.f), turning_force(0.f), speed(0.f), velocity(0.f), acceleration(0.f) { }
 
 		Vehicle(const Transform& transform, const vec4& colour, float turning_force, float speed)
-			: Drawable(transform, colour), turning_force(turning_force), speed(speed), velocity(0.f), acceleration(0.f), detected(false)
-		{
-			float sz = 50.f;
-
-			Transform left_sensor_transform = Transform{vec3{0.f}, vec3{sz, 0.f, sz}, 45.f};
-			left_sensor = Sensor(left_sensor_transform, {1.f, 1.f, 0.f, 1.f}, {1.f, 1.f});
-
-			Transform right_sensor_transform = Transform{vec3{0.f}, vec3{sz, 0.f, sz}, -45.f};
-			right_sensor = Sensor(right_sensor_transform, {1.f, 1.f, 0.f, 1.f}, {1.f, -1.f});
-		}
+			: Drawable(transform, colour), turning_force(turning_force), speed(speed), velocity(0.f), acceleration(0.f), detected(false) { }
 
 		std::vector<std::string> string_attribs();
 
 		void init(const mat4& projection_matrix);
 		void update(const maths::vec2& cursor_pos);
-		void draw_sensors(const mat4& view_matrix, const mat4& projection_matrix);
 		void draw(const mat4& view_matrix, const mat4& projection_matrix);
 		void destroy();
 
-		Draw_Line* dl;
-
-		Sensor left_sensor;
-		Sensor right_sensor;
-
 		bool detected;
-
-		vec2 direction;
-
-		// Physics
+	
 		float turning_force;
 		float speed;
+
 		vec2 velocity;
 		vec2 acceleration;
+		vec2 direction;
 
 		std::vector<vec2> other_vehicle_locations;
 	};
--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -17,9 +17,6 @@ namespace simulation {
 		
 		glEnableVertexAttribArray(1);
 		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
-
-		right_sensor.init(projection_matrix);
-		left_sensor.init(projection_matrix);
 	}
 
 
@@ -73,12 +70,6 @@ namespace simulation {
 		mat4 m = mult(mult(s, r), t);
 		shader.set_uniform("model", m);
 
-		/*vec3 camera_pos;
-		camera_pos.x = cos(utils::elapsed_time() * 0.5f) * 100.f;
-		camera_pos.y = 100.f + cos(utils::elapsed_time() * 0.2f) * 95.f;
-		camera_pos.z = sin(utils::elapsed_time() * 0.5f) * 100.f;*/
-		
-
 		shader.set_uniform("view", view_matrix);
 		shader.set_uniform("projection", projection_matrix);
 		shader.set_uniform("uniform_colour", colour);
@@ -89,18 +80,10 @@ namespace simulation {
 		glBindVertexArray(0);
 	}
 
-	void Vehicle::draw_sensors(const mat4& view_matrix, const mat4& projection_matrix) {
-		left_sensor.draw(view_matrix, projection_matrix);
-		right_sensor.draw(view_matrix, projection_matrix);
-	}
-
 	void Vehicle::destroy() {
 		glDeleteBuffers(1, &gl_buffer_object);
 		glDeleteVertexArrays(1, &gl_array_object);
 		shader.destroy();
-
-		left_sensor.destroy();
-		right_sensor.destroy();
 	}
 
 	std::vector<std::string> Vehicle::string_attribs() {
@@ -116,75 +99,3 @@ namespace simulation {
 		return attribs;
 	}
 }
-
-
-
-
-/*if (position.z > 50 || position.z < -50 || position.x > 50 || position.x < -50)
-rotation += turning_force;*/
-
-
-/*const static std::pair<vec2, vec2> top = {{-50.f, 50.f}, {50.f, 50.f}};
-const static std::pair<vec2, vec2> right = {top.second, {50.f, -50.f}};
-const static std::pair<vec2, vec2> bottom = {right.second, {-50.f, -50.f}};
-const static std::pair<vec2, vec2> left = {bottom.second, top.first};
-
-
-bool l = (
-left_sensor.intersects_line(top.first, top.second)
-|| left_sensor.intersects_line(right.first, right.second)
-|| left_sensor.intersects_line(bottom.first, bottom.second)
-|| left_sensor.intersects_line(left.first, left.second));
-
-if (l) {
-rotation += turning_force;
-speed += 0.002f;
-}*/
-
-/*vec2 vehicle_heading = polar_to_cartesian(to_radians(rotation));
-velocity = normalise(vehicle_heading);
-position += velocity * speed;
-
-
-
-left_sensor.parent_transform = {position, size, rotation};
-right_sensor.parent_transform = {position, size, rotation};
-
-left_sensor.update(cursor_pos);
-right_sensor.update(cursor_pos);*/
-
-/*other_vehicle_locations.push_back(cursor_pos);
-
-other_vehicle_locations.erase(std::remove_if(other_vehicle_locations.begin(), other_vehicle_locations.end(), [&](vec2 v) -> bool {
-return almost_equal(v, position.XZ(), 1.f);
-}), other_vehicle_locations.end());*/
-
-//bool l = (
-//	left_sensor.intersects_line(top.first, top.second)
-//	|| left_sensor.intersects_line(right.first, right.second)
-//	|| left_sensor.intersects_line(bottom.first, bottom.second)
-//	|| left_sensor.intersects_line(left.first, left.second)
-//	|| left_sensor.intersects(other_vehicle_locations));
-
-//bool r = (
-//	right_sensor.intersects_line(top.first, top.second)
-//	|| right_sensor.intersects_line(right.first, right.second)
-//	|| right_sensor.intersects_line(bottom.first, bottom.second)
-//	|| right_sensor.intersects_line(left.first, left.second)
-//	|| right_sensor.intersects(other_vehicle_locations));
-
-//if (l && r) {
-//	rotation += turning_force * 2.f;
-//	speed = max(speed - 0.004f, .5f);
-//} else if (l) {
-//	rotation += turning_force;
-//	speed += 0.002f;
-//} else if (r) { 
-//	rotation -= turning_force;
-//	speed += 0.002f;
-//}
-//else {
-//	//speed = max(speed - 0.001f, .5f);
-//	rotation += 2;
-//	//speed += 0.002f;
-//}
